578ec1c21d23ff263985a6dc61663d83661a531d
Gabby Baniqued <gbaniqueds@gmail.com>
2023-10-13 16:38:59 -0400

    Created Parent class

--- src/bingo_cards/BingoCard.java
+++ src/bingo_cards/BingoCard.java
@@ -2,6 +2,21 @@ package bingo_cards;
 
 public class BingoCard {
 
-	//test
+	private static final int DEFAULT_GRID_SIZE = 5;	
+	
+	private int[][] bingoCard;
+	private int customGridSize;
+	
+	// default constructor with default grid size
+	public BingoCard() {
+		bingoCard = new int[DEFAULT_GRID_SIZE][DEFAULT_GRID_SIZE];
+		customGridSize = -1;
+	}
+	
+	// constructor with custom grid size
+	public BingoCard(int s) {
+		customGridSize = s;
+		bingoCard = new int[s][s];
+	}
 	
 }
